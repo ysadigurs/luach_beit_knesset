d1a499015745c92400185314353550b53988189e
ysadigurs <yona.sadigurs@gmail.com>
2024-07-30 12:31:56 +0300

    displayZamanimLeibovitz take #1

--- script.js
+++ script.js
@@ -69,6 +69,34 @@ function displayZmanim() {
     });
 }
 
+function displayZamanimLeibovitz() {
+    fetch('zmanim.json')
+    .then(response => response.json())
+    .then(data => {
+        const today = getTodayDate();
+        document.getElementById('chatzotNight').textContent = `${data.items.find( record => (record.category === "חצות" && record.date === today)).substr(0,5)}`;
+        document.getElementById('alotHaShachar').textContent = `${data.items.find( record => (record.category === "עלות השחר" && record.date === today)).substr(0,5)}`;
+        document.getElementById('misheyakir').textContent = `${data.items.find( record => (record.category === "זמן ציצית" && record.date === today)).substr(0,5)}`;
+        
+/*
+        document.getElementById('sunrise').textContent = `${data.times.sunrise.substr(11, 5)}`;
+        document.getElementById('sofZmanShmaMGA').textContent = `${data.times.sofZmanShmaMGA.substr(11, 5)}`;
+        document.getElementById('sofZmanShma').textContent = `${data.times.sofZmanShma.substr(11, 5)}`;
+        document.getElementById('sofZmanTfilla').textContent = `${data.times.sofZmanTfilla.substr(11, 5)}`;
+        document.getElementById('chatzot').textContent = `${data.times.chatzot.substr(11, 5)}`;
+        document.getElementById('minchaGedola').textContent = `${data.times.minchaGedola.substr(11, 5)}`;
+        document.getElementById('minchaKetana').textContent = `${data.times.minchaKetana.substr(11, 5)}`;
+        document.getElementById('plagHaMincha').textContent = `${data.times.plagHaMincha.substr(11, 5)}`;
+        document.getElementById('sunset').textContent = `${data.times.sunset.substr(11, 5)}`;
+        document.getElementById('tzeit7083deg').textContent = `${data.times.tzeit7083deg.substr(11, 5)}`;
+*/
+        
+    })
+    .catch(error => {
+        console.error('Error fetching the Zmanim:', error);
+    });
+}
+
 function displayShabbatHours() {
     fetch('https://www.hebcal.com/shabbat?cfg=json&i=on&geonameid=8199379&ue=off&b=28&c=on&M=on&F=on&lg=he&tgt=_top')
     .then(response => response.json())
@@ -152,7 +180,8 @@ function displayChagim() {
 function initApp () {
     setInterval(updateClock, 1000);
     updateClock();
-    displayZmanim();
+    //displayZmanim();
+    displayZamanimLeibovitz();
     displayShabbatHours();
     displayShabbatStatic();
     displayShiurim();
