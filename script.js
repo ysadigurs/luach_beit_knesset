4a542d2ed6e2ca3a17e00031753861a6aabcfb1f
ysadigurs <yona.sadigurs@gmail.com>
2024-07-31 14:25:14 +0300

    f

--- script.js
+++ script.js
@@ -200,7 +200,7 @@ function displayChagim() {
 
 function initApp () {
     // Reload the page every 60 seconds (60000 milliseconds)
-    setInterval(() => {location.reload();}, 60000);
+    //setInterval(() => {location.reload();}, 60000);
     setInterval(updateClock, 1000);
     updateClock();
     displayLeibovitzZmanim(); 
