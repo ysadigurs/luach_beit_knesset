64b0c8a6f99b37ced5fd87668b55b836894af8a4
ysadigurs <yona.sadigurs@gmail.com>
2024-08-18 20:54:12 +0300

    fetch

--- script.js
+++ script.js
@@ -248,13 +248,7 @@ function autoScroll() {
 // 
 
 function displayWeather () {
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=31.8686&longitude=34.9889&current_weather=true" , {
-        method: 'GET', // Use the appropriate HTTP method
-        headers: {
-          'Content-Type': 'application/json', // Optional: Only necessary if you're sending a request body
-          'Origin': 'https://ysadigurs.github.io'
-        }
-    })   
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=31.8686&longitude=34.9889&current_weather=true")
     .then(response => {
         const result = response.json();
         document.getElementById('temperature').textContent = `${result.current_weather.temperature}` + " מעלות ";
