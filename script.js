0827f6b9e41eb14b4df0a319b0aa715a84c24248
ysadigurs <yona.sadigurs@gmail.com>
2024-09-12 09:12:24 +0300

    zmanim daily

--- script.js
+++ script.js
@@ -82,6 +82,39 @@ function getNextSaturday() {
     return nextSaturday;
 }
 
+/* 
+ * Daily Zmanim
+ * Shaalabim geonameid=293590
+ * Nof Hayalom geonameid=8199379
+ */
+function displayZmanim() {
+    fetch('https://www.hebcal.com/zmanim?cfg=json&geonameid=293590')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('chatzotNight').textContent = `${data.times.chatzotNight.substr(11, 5)}`;
+        document.getElementById('alotHaShachar').textContent = `${data.times.alotHaShachar.substr(11, 5)}`;
+        document.getElementById('misheyakir').textContent = `${data.times.misheyakir.substr(11, 5)}`;
+        document.getElementById('sunrise').textContent = `${data.times.sunrise.substr(11, 5)}`;
+        document.getElementById('sofZmanShmaMGA').textContent = `${data.times.sofZmanShmaMGA.substr(11, 5)}`;
+        document.getElementById('sofZmanShma').textContent = `${data.times.sofZmanShma.substr(11, 5)}`;
+        document.getElementById('sofZmanTfilla').textContent = `${data.times.sofZmanTfilla.substr(11, 5)}`;
+        document.getElementById('chatzot').textContent = `${data.times.chatzot.substr(11, 5)}`;
+        document.getElementById('minchaGedola').textContent = `${data.times.minchaGedola.substr(11, 5)}`;
+        //document.getElementById('minchaKetana').textContent = `${data.times.minchaKetana.substr(11, 5)}`;
+        //document.getElementById('plagHaMincha').textContent = `${data.times.plagHaMincha.substr(11, 5)}`;
+        document.getElementById('sunset').textContent = `${data.times.sunset.substr(11, 5)}`;        
+                
+    })
+    .catch(error => {
+        console.error('Error fetching the Zmanim:', error);
+    });
+    
+    console.log('displayZmanim() ends');        
+ 
+}
+
+
+
 console.log(getNextSaturday());
 let parasha_date = null;
 
@@ -114,17 +147,17 @@ function displayLeibovitzZmanim() {
             document.getElementById('motzash').textContent = `${item["motzash"].substr(0, 5)}`;
             
             // Zmanim (weekly)
-            document.getElementById('chatzotNight').textContent = `${item["hazot"].substr(0, 5)}`;  
-            document.getElementById('alotHaShachar').textContent = `${item["alot"].substr(0, 5)}`;
-            document.getElementById('misheyakir').textContent = `${item["tzitzit"].substr(0, 5)}`;
-            document.getElementById('sunrise').textContent = `${item["netz"].substr(0, 5)}`;
-            document.getElementById('sofZmanShmaMGA').textContent = `${item["kshmamagen"].substr(0, 5)}`;
-            document.getElementById('sofZmanShma').textContent = `${item["kshmaagra"].substr(0, 5)}`;
-            document.getElementById('sofZmanTfilla').textContent = `${item["tfilaagra"].substr(0, 5)}`;
-            document.getElementById('chatzot').textContent = `${item["hazot"].substr(0, 5)}`;                      
-            document.getElementById('minchaGedola').textContent = `${item["minchagdola"].substr(0, 5)}`;
+            //document.getElementById('chatzotNight').textContent = `${item["hazot"].substr(0, 5)}`;  
+            //document.getElementById('alotHaShachar').textContent = `${item["alot"].substr(0, 5)}`;
+            //document.getElementById('misheyakir').textContent = `${item["tzitzit"].substr(0, 5)}`;
+            //document.getElementById('sunrise').textContent = `${item["netz"].substr(0, 5)}`;
+            //document.getElementById('sofZmanShmaMGA').textContent = `${item["kshmamagen"].substr(0, 5)}`;
+            //document.getElementById('sofZmanShma').textContent = `${item["kshmaagra"].substr(0, 5)}`;
+            //document.getElementById('sofZmanTfilla').textContent = `${item["tfilaagra"].substr(0, 5)}`;
+            //document.getElementById('chatzot').textContent = `${item["hazot"].substr(0, 5)}`;                      
+            //document.getElementById('minchaGedola').textContent = `${item["minchagdola"].substr(0, 5)}`;
             //document.getElementById('plagHaMincha').textContent = `${item["minchahol"]}`; -- missing plag mincha
-            document.getElementById('sunset').textContent = `${item["shkia"].substr(0, 5)}`;
+            //document.getElementById('sunset').textContent = `${item["shkia"].substr(0, 5)}`;
             document.getElementById('tzeit').textContent = `${item["tzeet"].substr(0, 5)}`;
             
             // Tfila Hol
@@ -271,6 +304,7 @@ function displayAll () {
     console.log("Function called at: " + currentTime);
 
     displayLeibovitzZmanim();    
+    displayZmanim();    
     displayShabbatStatic();
     displayConfig();
     displayChol();
