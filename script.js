840c3ca5dc662a8b98926d10cea268091b64d901
ysadigurs <yona.sadigurs@gmail.com>
2024-12-08 21:38:21 +0200

    script

--- script.js
+++ script.js
@@ -299,33 +299,13 @@ function displayConfig() {
         document.getElementById('shacharit_chol_3').textContent = `${data["shacharit_chol_3"]}`;    
         document.getElementById('mincha_gdola_chol').textContent = `${data["mincha_gdola_chol"]}`;   
     
-        const currentDay = getCurrentDay();
-        if (currentDay === "Tuesday" || currentDay === "Wednesday" || currentDay === "Thursday" || currentDay === "Friday" || currentDay === "Saturday" ) {
-        
-            // Read config json data            
-            document.getElementById('dvar_tora').textContent = `${data["dvarTora"]}`;
-            document.getElementById('shiur_tfila_time').textContent = `${data["shiurAfterTfilaTime"]}`;
-            document.getElementById('shiur_tfila').textContent = `${data["shiurAfterTfila"]}`;        
-            document.getElementById('shiur_shabat_time').textContent = `${data["shiurShabatTime"]}`;
-            document.getElementById('shiur_shabat_name').textContent = `${data["shiurShabatName"]}`;
-            //document.getElementById('shiur_shabat').textContent = `${data["shiurShabatTitle"]}`;            
-
-        }
-        else {
-
-            // Clear config data in the begining of the week
-            document.getElementById('dvar_tora').textContent = "";
-            document.getElementById('shiur_tfila_time').textContent = "";
-            document.getElementById('shiur_tfila').textContent = "";
-            document.getElementById('shiur_shabat_time').textContent = "";
-            document.getElementById('shiur_shabat_name').textContent = "";
-            document.getElementById('shiur_shabat').textContent = "";
-        }
-        
-        
-
-       
-               
+        // Read config json data            
+        document.getElementById('dvar_tora').textContent = `${data["dvarTora"]}`;
+        document.getElementById('shiur_tfila_time').textContent = `${data["shiurAfterTfilaTime"]}`;
+        document.getElementById('shiur_tfila').textContent = `${data["shiurAfterTfila"]}`;        
+        document.getElementById('shiur_shabat_time').textContent = `${data["shiurShabatTime"]}`;
+        document.getElementById('shiur_shabat_name').textContent = `${data["shiurShabatName"]}`;
+        //document.getElementById('shiur_shabat').textContent = `${data["shiurShabatTitle"]}`;                        
                  
     })
     .catch(error => {
